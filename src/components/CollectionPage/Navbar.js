bf8e23038bbd37e6e0827267e4e724d8c572ffa2
TvickyPace <tvicky.pacefin@gmail.com>
2024-12-21 23:54:15 +0530

    feat: navbar is responsive

--- src/components/CollectionPage/Navbar.js
+++ src/components/CollectionPage/Navbar.js
@@ -40,11 +40,11 @@ function Navbar({
           width={118}
           height={26}
           priority={true}
-          className="absolute left-[33px] top-[32px] aspect-auto w-[90px]"
+          className="absolute left-[33px] top-[32px] aspect-auto w-[90px] max-w-1023-max-w-736:left-1/2 max-w-1023-max-w-736:top-[20px] max-w-1023-max-w-736:w-[76px] max-w-1023-max-w-736:-translate-x-1/2"
         />
         <p
           onClick={() => setOpen(true)}
-          className={`${customizeActive ? "block" : "hidden"} absolute left-1/2 top-[32px] flex -translate-x-1/2 cursor-pointer items-center gap-x-[5.2px] text-[17px] leading-[25px] tracking-[-0.37px]`}
+          className={`${customizeActive ? "block" : "hidden"} absolute left-1/2 top-[32px] flex -translate-x-1/2 cursor-pointer items-center gap-x-[5.2px] text-[17px] leading-[25px] tracking-[-0.37px] max-w-1023-max-w-736:left-[72px] max-w-1023-max-w-736:top-[57px]`}
         >
           {content?.collection?.para}
           <Image
@@ -57,7 +57,7 @@ function Navbar({
           />
         </p>
         <button
-          className={`${customizeActive ? "block" : "hidden"} animate-showAnimation1d8s absolute right-[18px] top-[24px] rounded-full bg-primaryButtonColor px-[16px] py-[9px] text-[13.67px] leading-[18px] tracking-[-0.22px] text-white opacity-0 hover:bg-primaryButtonColor/[0.97]`}
+          className={`${customizeActive ? "block" : "hidden"} absolute right-[18px] top-[24px] animate-showAnimation1d8s rounded-full bg-primaryButtonColor px-[16px] py-[9px] text-[13.67px] leading-[18px] tracking-[-0.22px] text-white opacity-0 hover:bg-primaryButtonColor/[0.97] max-w-1023-max-w-736:top-[48px]`}
         >
           {content?.save?.para}
         </button>
